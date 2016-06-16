fcc2318d2cbe8aef76c40f36373f3800ed7efacf
mattchew <mlapehn@gmail.com>
2016-06-16 08:36:07 -0600

    functions1

--- starter-code/functions.js
+++ starter-code/functions.js
@@ -1,21 +1,25 @@
 // Question 1
 function maxOfTwoNumbers() {
-
+Math.max(5,10)
 }
 
 // Question 2
 function maxOfThree() {
-
+Math.max(-10,7,5)
 }
 
 // Question 3
 function isCharacterAVowel() {
-
+function isVowel(c) {
+    return ['a', 'e', 'i', 'o', 'u'].indexOf(c.toLowerCase()) !== -1
 }
 
 // Question 4
 function sumArray() {
- 
+ var numbers = [15.5, 2.3, 1.1, 4.7];
+
+function getSum(total, num) {
+    return total + Math.round(num);
 }
 
 
@@ -26,16 +30,22 @@ function multiplyArray() {
 
 
 // Question 5
-var numberOfArguments = function(){
+var numberOfArguments = function()
   
-}
+
 
 
 
 // Question 6
-var reverseString = function (){
-  
+var reverseString = function ()(str){
+  var arr = str.split('');
+  var revArr = [];
+  for (var i = arr.length - 1; i >= 0; i--) {
+    revArr.push(arr[i]);
+  }
+  return revArr.join('');
 };
+
 
 
 // Question 7
